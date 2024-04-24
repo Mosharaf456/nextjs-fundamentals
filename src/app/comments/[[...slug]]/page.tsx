6c5f196ee9da5c23a5e6dd72567dcb93ef0dfdfe
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-24 13:10:25 +0600

    ref optinal catch all route added

---
+++ src/app/comments/[[...slug]]/page.tsx
@@ -0,0 +1,26 @@
+/* any url can catch that contain /comments/* 
+length 2 means 2 segments in the URL
+http://localhost:3000/comments/feature1/concept1
+optinal catch all route [[...slug]]
+optinal catch al URL:
+http://localhost:3000/comments
+
+*/
+export default function Comments({ params }: {
+    params: {
+        slug: string[]
+    }
+}) {
+    if(params.slug?.length === 2) {
+        return <h1>
+            Comment home page feature{params.slug[0]} concept {params.slug[1]}
+        </h1>
+    } else if(params.slug?.length === 1) {
+        return <h1>
+            Comment home page feature {params.slug[0]}
+        </h1>
+    }
+    return (
+        <h1>Comment home page</h1>
+    )
+}
