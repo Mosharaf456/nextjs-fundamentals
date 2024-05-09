8f999cb0d359199593de2a5d87cd499c22ce3479
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-09 14:21:22 +0600

    ref headers and cookie set/get on router handlers

--- src/app/(route-handlers)/comments-two/[id]/route.ts
+++ src/app/(route-handlers)/comments-two/[id]/route.ts
@@ -2,6 +2,35 @@
 context is an object that contains the request object and other properties
 since we are not using parameter request so we can prefix it with underscore(_) 
 
+
+*** Headers in route handlers
+HTTP headers represent the metadata associated with an API request and response.
+
+Request Headers:
+1. Request headers are used to provide additional information about the request that send by client such as a browser.
+2. Request headers are sent by the client to the server.
+'User-Agent' which identifies the browser and operating system to the server.
+'Accept' which specifies the media types that the client can understand.Like text,video or image formats that client can process.
+'Authorization' which is used to authenticate the client to the server.
+Response Headers:
+1. Response headers that sent back from server to client . The provide additional information about the server's and the data being sent in response.
+
+Content-Type: 
+Indicates media type of the response.
+What data format the response is in to the client from server
+such as text/html for HTML ,documents, application/json for JSON data etc, XML, or plain text.
+
+
+
+Content-Length: 1234
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH
+Access-Control-Allow-Headers: Content-Type
+Access-Control-Max-Age: 86400
+Access-Control-Allow-Credentials: true
+Access-Control-Expose-Headers: Content-Length
+
+How to read request header and send back response headers in NEXT JS.?
 */
 
 import { comments } from "../data";
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,43 @@
-export async function GET() {
-    return new Response("Profile data");
+/*
+Cookies are small pieces of data that a server sends to user,s web browser.
+The browser may store the cookie and send it back to same server with the later request.
+Cookies are used mainly three purposes:
+1. Session management : like login, session card, game score etc.
+2. Personalization: like user preferences, themes, and other settings.
+3. Tracking : like recording and analyzing user behavior.
+
+How to set/get cookies in NEXT JS.?
+
+
+*/
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // header read
+    const requestHeaders = new Headers(request.headers);
+    console.log("requestHeaders", requestHeaders.get("Authorization"));
+    const headerList = headers();
+    console.log("headerList", headerList.get("Authorization"));
+    //header read end
+
+    // set cookie by next js
+    cookies().set("resultPerPage", "20");
+
+    // read cookie by next js 
+    const cookiesList = cookies().get("resultPerPage");
+    console.log("cookiesList", cookiesList);
+
+    // read cookie
+    const theme = request.cookies.get("theme");
+    console.log("theme", theme);
+
+    // header, cookie write
+    return new Response("<h1> Hello, Profile route handler data! </h1>", {  
+        headers: { 
+            "Content-Type": "text/html" ,
+            "Set-Cookie": "theme=dark; SameSite=Strict; Secure; HttpOnly; Path=/; Expires=Wed, 09 Jun 2021 10:18:14 GMT"
+        },
+    });
 }
+
