a2c4b3d580681b47b0f7a6d6fa5ac7923005d4f0
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-09 17:09:33 +0600

    ref topic rendering comment added

--- src/middleware_back.ts
+++ src/middleware_back.ts
@@ -74,5 +74,48 @@ Parallel and intercepting routes
 Route handlers-- helps define restful API endpoint and middleware 
 Cookies and headers
 
+
+
+*****Rendering*******
+Is the process that trnsforms the code you write into UI .
+
+Choosing rendering is vital for building a performant application 
+- Server side rendering
+- Static site generation
+- Client side rendering
+
+CSS, SSR, and RSCs
+
+rendering in react
+Rendering in react is the process of converting react components into DOM elements 
+that are displayed in the browser.
+In react Client request -- and server response simple html div root element + JS reference that is bundle.js 
+
+request js bundle.js  it is downloaded when html file is parsed 
+Downloaded JS code then generate html code on your pc and insert it into the DOM under the root div element and you see the UI
+in the browser. The evidence is inspect element in the browser that will show all html but not in the view source option.
+This is called client side rendering(CSR)
+This is the standard of SPAs with widespread adoption.
+
+Draw back: 
+dingle div tag that is not optimal for SEO.
+As it is provides little content for search engine to index.
+Large bundle size that can slow down the initial load time.
+Waterfall of network req for API responses from deeply nested components may result 
+meaningfull content is not being rendered fast enough for a crawler to index it.
+
+Fetching data computing UI and making HTML interactive can slow things down.
+Users might see a blank screen or suspense loading spinner while page loads.
+
+Each new feature  added to the app  increases the size of js bundle, prolonging the wait time
+for users to see the UI.
+It laid the ground work for interactive web app we see today but to enahance SEO and performance
+developers started looking for better solutions like SSR.And alos it is limitations of CSR.
+
+This is where server side rendering(SSR) comes in.
+
+
+
+
 */
 
