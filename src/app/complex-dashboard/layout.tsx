07cdb6cca5954e3d42656e351915ad9e041e9225
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-09 09:21:09 +0600

    ref spelling correction

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,9 +1,9 @@
  /*
- In Next.js the app router introduced twon advance routing
+ In Next.js the app router introduced two advance routing
  1. Parallel routes
  2. Intercepting routes
   ***Intercepting routes****
-  Intercepting routes allows us to intercept or stop the default routing behaviour to
+  Intercepting routes allows us to intercept or stop the default routing behavior to
   present an alternative view or component when navigating through the UI, while still preserving
   the intended route for scenarios like page reloads.
   This can be useful if you want to show a route while keeping the context of the 
