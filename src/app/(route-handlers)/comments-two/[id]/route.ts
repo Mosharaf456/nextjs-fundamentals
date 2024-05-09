7781ad446a7f70bed7921ea027333c53eed2449c
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-09 12:36:41 +0600

    ref dynamic route handler created: for PUT,PATCH,DELETE,GET

--- src/app/(route-handlers)/comments-two/[id]/route.ts
+++ src/app/(route-handlers)/comments-two/[id]/route.ts
@@ -14,3 +14,51 @@ export async function GET(
     }
     return Response.json(comment);
 }
+
+
+export async function PATCH(
+    request: Request, 
+    { params }: { params: { id: string } }
+) {
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    if (index === -1) {
+        return new Response("Comment not found", { status: 404 });
+    }
+    comments[index].text = text;
+
+    return Response.json(comments[index]);
+    
+}
+
+export async function DELETE(
+    _request: Request, 
+    { params }: { params: { id: string } }
+) {
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    if (index === -1) {
+        return new Response("Comment not found", { status: 404 });
+    }
+    comments.splice(index, 1);
+
+    return new Response("Comment deleted", { status: 200 });
+}
+
+export async function PUT(
+    request: Request, 
+    { params }: { params: { id: string } }
+) {
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    if (index === -1) {
+        return new Response("Comment not found", { status: 404 });
+    }
+    comments[index].text = text;
+
+    return Response.json(comments[index]);
+    
+}
+
+
