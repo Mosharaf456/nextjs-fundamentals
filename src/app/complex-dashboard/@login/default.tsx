26710ad464b200668e630962b85967551d4dc99f
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-09 10:46:16 +0600

    ref autheticated parallel route login page UI change:DONE

--- src/app/complex-dashboard/@login/default.tsx
+++ src/app/complex-dashboard/@login/default.tsx
@@ -2,6 +2,15 @@ import Card from "@/components/Card";
 
 export default function DefaultLogin() {
     return (
-        <Card>Default Login Page</Card>
+        <Card>
+            <h1>Login page</h1>
+            <input type='text' placeholder='Username' />
+            <br />
+            <br />
+            <input type='password' placeholder='Password' />
+            <br />
+            <br />
+            <button>Login</button>
+        </Card>
     );
 }
