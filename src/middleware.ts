d93de6e1105671ea7c7780170a4cf96f3efe9e7b
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-16 11:24:54 +0600

    ref Interleaving Page implementation details of explaination added:updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -128,6 +128,11 @@ So it,s recomended to position these client components lower in your component t
 ***** Interleaving Server and Client Components 
 - interleaving
 
+Any component inside server component can call both client and server component.
+But inside client component can,t call server component.
+Because client component automatically convert server to client component when is it called inside client component.
+It thriggers the error. Because server component rendered before client component so you can,t call server component inside client component.
+Not supported next js. You can pass it as a props to the client component. React children props. 
 
 
 
